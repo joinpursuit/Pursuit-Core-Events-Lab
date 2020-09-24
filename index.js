65c62b7ea17acfb03dd5d830b7398bc86cb163fa
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-24 16:22:22 -0400

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Code your solution!")
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector("#button")
+  button.addEventListener('click', addTodo)
+})
+
+function addTodo() {
+  const input = document.querySelector('#todo-input')
+  const todoText = input.value
+  const newListItem = document.createElement("li")
+  newListItem.innerText = todoText
+  const list = document.querySelector('#todo-list')
+  list.appendChild(newListItem)
+}
