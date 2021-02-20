98f2c75414bb0a6eb60825d06a3782e0beae0e9b
Marsha Hall <mphall77@gmail.com>
2021-02-20 15:39:40 -0500

    add error message

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const form = document.querySelector("form")
     form.addEventListener("submit", event => {
         event.preventDefault();
-        const input = document.querySelector("input").value
+        let input = document.querySelector("input").value
         const li = document.createElement("li")
         li.textContent = input 
         document.querySelector("ul").appendChild(li)
+        
+        if (input === "") {
+            const p = document.querySelector("p");
+            p.textContent = "Error. Todo cannot be empty";
+            
+        }
+        input.value = "";
     })
 })
