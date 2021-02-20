b848c1630fc29e0f855f03d7ef92fd910f5e2b69
Auredy Sanchez <auredysanchez@gmail.com>
2021-02-20 16:20:57 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#todo-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("#to-do-list");
+    let li = document.createElement("li");
+    li.textContent = input.value;
+    const ul = document.querySelector("ul");
+    ul.appendChild(li);
+    const liText = document.querySelector("li")
+    liText.addEventListener("click", (event) => {
+      liText.style.textDecoration = "line-through"
+  });
+});
+});
+
