40133e1e34e1fcad1043f12add9d54d9152083c1
Shoaib Dar <SDAR30@gmail.com>
2021-02-20 17:14:08 -0500

    first 3 bonus questions

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 // console.log("Code your solution!")
 const form = document.querySelector("form")
-form.addEventListener("submit",(e)=>{
-e.preventDefault()
-let input = document.querySelector("input")
-let li = document.createElement("li")
-let deleteButton = document.createElement("button")
-li.textContent = input.value + " "
-deleteButton.textContent = "Delete"
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let textArea = document.querySelector("textarea")
+    let li = document.createElement("li")
+    let deleteButton = document.createElement("button")
 
-li.appendChild(deleteButton)
-let allItems = document.querySelector("#all-items")
-allItems.appendChild(li)
-input.value = ""
+    li.textContent = textArea.value + " "
+    deleteButton.textContent = "Delete"
+    deleteButton.id = "delete-button"
+
+    li.appendChild(deleteButton)
+    let allItems = document.querySelector("#all-items")
+    allItems.appendChild(li)
+    textArea.value = ""
+
+    deleteButton.addEventListener("click", (e) => {
+        deleteButton.parentNode.remove()
+    })
+
+
 })
 
 let allItems = document.querySelector("#all-items")
-allItems.addEventListener("click", (e)=>{
-    e.target.style.textDecoration = "line-through";
-
+allItems.addEventListener("click", (e) => {
+    if (e.target.style.textDecoration === "line-through") {
+        e.target.style.textDecoration = "none"
+    } else {
+        e.target.style.textDecoration = "line-through"
+    }
 })
 
 
