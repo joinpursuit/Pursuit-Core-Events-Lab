fd5ef5ee433edf79128233bb74abae8bb8a1a525
Demi <oluwademiladejobi@pursuit.org>
2021-02-20 15:59:24 -0500

    Added error message -disappears when correct input

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-})
-
 const addToList = () => {
     let input = document.querySelector("#list-adder")
     const ul = document.querySelector("ul")
     inputText = input.value
     li = document.createElement("li")
-    ul.appendChild(li)
-    li.textContent = inputText
-    
+    p = document.getElementById("error")
+    if(input.value === ""){
+        p.textContent = "Error! Todo cannot be empty!"
+    }else{
+        ul.appendChild(li)
+        li.textContent = inputText;
+        input.value = "";   
+        p.textContent = "";
+        // Want to remove Error message when correct input is given
+
+    }
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToList()
 })
+form.addEventListener("click", (e))
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+// })
