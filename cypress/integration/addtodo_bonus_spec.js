9e239339a158542536be2e0a90d516fee2a3924f
sparky evangelista <Sparkyevangelista@gmail.com>
2021-02-20 16:41:37 -0500

    Updated tests
    * Added " input " inside all of the gets that need to reference it to be cleared
    - We don't use textarea, I don't think it should be there

--- cypress/integration/addtodo_bonus_spec.js
+++ cypress/integration/addtodo_bonus_spec.js
@@ -1,12 +1,12 @@
 describe("Todo App - Bonus", () => {
   it("clears the input when a todo is added", () => {
-    cy.get("textarea").clear().type("Wash dishes");
+    cy.get("textarea, input").clear().type("Wash dishes");
     cy.get("button[type=submit]").click();
     cy.get("textarea").should("have.value", "");
   });
 
   it("deletes only one item when its delete button is pressed", () => {
-    cy.get("textarea").clear().type("Wash dishes");
+    cy.get("textarea, input").clear().type("Wash dishes");
     cy.get("button[type=submit]").click();
     cy.get("textarea").clear().type("Be awesome");
     cy.get("button[type=submit]").click();
@@ -18,7 +18,7 @@ describe("Todo App - Bonus", () => {
   });
 
   it("no longer strikes through an li when clicked a second time", () => {
-    cy.get("textarea").clear().type("Wash dishes");
+    cy.get("textarea, input").clear().type("Wash dishes");
     cy.get("button[type=submit]").click();
     cy.get("li")
       .first()
@@ -28,7 +28,7 @@ describe("Todo App - Bonus", () => {
   });
 
   it("adds multiple to-dos when the text input has multiple lines", () => {
-    cy.get("textarea").clear().type("Wash dishes\nBe awesome");
+    cy.get("textarea, input").clear().type("Wash dishes\nBe awesome");
     cy.get("button[type=submit]").click();
     cy.get("li").should(($li) => {
       expect($li).to.have.length(2);
