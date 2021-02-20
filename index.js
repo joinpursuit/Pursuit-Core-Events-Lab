72efab261cb2826a4d83ce27ddfcf29b8f699dec
Jordan Bobadilla <jordanboba08@gmail.com>
2021-02-20 15:24:31 -0500

    lists created

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Code your solution!")
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    const form = document.querySelector("form")
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        const input = document.querySelector("input").value
+        const li = document.createElement("li")
+        li.textContent = input 
+        document.querySelector("ul").appendChild(li)
+    })
+})
