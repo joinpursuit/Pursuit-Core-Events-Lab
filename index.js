88907e4ddc4bce0a0591c849d22586b5ee9c0dc2
Miguel Garcia <miguelgarcia@pursuit.org>
2021-02-20 15:39:12 -0500

    able to add to ToDoList

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("Code your solution!")
+let ul = document.getElementById("unordered-list")
+
+button.addEventListener("click", () => {
+
+    let input = document.getElementById("text-input")
+    let input1 = input.value
+    
+    debugger
+    let li = document.createElement("li")
+    debugger
+    li.textContent = input1
+    debugger
+    ul.appendChild(li)
+})
+
+
+
+
+// let list = document.querySelector("#user-list")
+
+// for (let i = 0; i < data.results.length; i++) {
+//     let li = document.createElement("li");
+//     let nationality = data.results[i].nat
+//     let firstName = data.results[i].name.first
+//     let lastName = data.results[i].name.last
+//     li.textContent = `${firstName} ${lastName} (${nationality})`;
+//     list.appendChild(li)
