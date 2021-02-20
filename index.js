7604b74fe0047231e1ca9730c8a39664f92dd2c8
Kobe <tahiriadam@gmail.com>
2021-02-20 15:46:58 -0500

    finished first part of lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const ul = document.querySelector("ul");
+  const input = document.querySelector("input");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const listItem = document.createElement("li");
+    listItem.textContent = input.value;
+    ul.appendChild(listItem);
+    listItem.addEventListener("click", (event) => {
+        listItem.style.textDecoration = "line-through"
+    })
+  });
+});
