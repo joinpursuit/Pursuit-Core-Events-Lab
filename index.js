6b7697337cecb8e2d4f5fafe860e7bfe0f2b307a
joshuamendez <joshuafmendez@gmail.com>
2021-02-20 15:07:42 -0500

    partial completed 2nd bullet

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     let form = document.querySelector('#todo_list_form');
     form.addEventListener('submit', (event) => {
-        event.preventDefault()
+        event.preventDefault();
         let todo = document.querySelector('#todo_list')
         let input = document.querySelector('#add_to_todo_list');
         let item = input.value;
-        let li = document.createElement('li');
-        li.classList.add('list_item');
-        li.textContent = item;
-        todo.appendChild(li);
-        input.value = '';
-    })
+        if(item){
+            let li = document.createElement('li');
+            li.classList.add('list_item');
+            li.textContent = item;
+            todo.appendChild(li);
+            input.value = '';
+        } else {
+            let body = document.querySelector('body');
+            let p = document.createElement('p');
+            p.classList.add('error_message');
+            let errorMessage = 'Error! Todo cannot be empty!'
+            p.textContent = errorMessage;
+            body.appendChild(p);
+        }
+    });
 });
