43709d8d62ce1b2f90f86630ba6650ed92cc9193
joshuamendez <joshuafmendez@gmail.com>
2021-02-20 11:01:17 -0500

    implemented todo list and add items to it

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Code your solution!")
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('#todo_list_form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let todo = document.querySelector('#todo_list')
+        let input = document.querySelector('#add_to_todo_list');
+        let item = input.value;
+        let li = document.createElement('li');
+        li.classList.add('list_item');
+        li.textContent = item;
+        todo.appendChild(li);
+        input.value = '';
+    })
+});
