6924f4bc9cb164196b04054f88cfb9f1156ef180
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-02-20 15:46:40 -0500

    will complete lab and pass all specs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+const form = document.querySelector("form")
+form.addEventListener("submit",(e)=>{
+e.preventDefault()
+let input = document.querySelector("input")
+let li = document.createElement("li")
+let deleteButton = document.createElement("button")
+li.textContent = input.value + " "
+deleteButton.textContent = "Delete"
+
+li.appendChild(deleteButton)
+let allItems = document.querySelector("#all-items")
+allItems.appendChild(li)
+input.value = ""
+})
+
+let allItems = document.querySelector("#all-items")
+allItems.addEventListener("click", (e)=>{
+    e.target.style.textDecoration = "line-through";
+
+})
+
+
