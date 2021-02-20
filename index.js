bdcd3218ebbfdbd6d71da6edb3b141c73d18fc98
Marsha Hall <mphall77@gmail.com>
2021-02-20 17:10:48 -0500

    bonus fix the specs

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
 
 document.addEventListener("DOMContentLoaded", (event) => {
+	let input;
 
-    let input;
-    const form = document.querySelector("form")
-    form.addEventListener("submit", event => {
-        event.preventDefault();
-        
-        input = document.querySelector("input").value
-        let li = document.createElement("li")
-        let p = document.querySelector("p");
-        
-        if (input === "") {
-            p.textContent = "Error. Todo cannot be empty";
-            
-        } else {
-            
-            li.textContent = input 
-            document.querySelector("ul").appendChild(li)
+	const form = document.querySelector("form");
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
 
-        }
-        li.addEventListener("click", () => {
-            li.style.textDecoration = "line-through" 
-        })
+		input = document.querySelector("input").value;
+
+		let li = document.createElement("li");
+		let p = document.querySelector("p");
+        let button = document.createElement("button");;
+
+		if (input === "") {
+			p.textContent = "Error. Todo cannot be empty";
+		} else {
+			li.textContent = input;
+            document.querySelector("ul").appendChild(li);
+            // need to attach the button to each li as it is created
+			button.textContent = "remove item";
+			document.querySelector("li").appendChild(button);
+		}
+input.value = " ";
+		li.addEventListener("click", () => {
+			li.style.textDecoration = "line-through";
+        });
         
-    })
-})
+        button.addEventListener("click", () => {
+            button.parentNode.remove();
+				});
+
+	});
+
+	// reset the input value to empty string
+	// 
+});
