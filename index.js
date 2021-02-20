938ce0f693931b011a82f9b6dca2f18e2c438b53
Coreen Cooper <coreencooper@pursuit.org>
2021-02-20 15:54:56 -0500

    add a new to-do to the list

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Code your solution!")
+const ul = document.querySelector("#unorder-list");
+const li = document.createElement("li");
+const submit = document.querySelector("button");
+submit.addEventListener("click", (event) => {
+    event.preventDefault()
+    const input = document.querySelector("input").value;
+    li.textContent = input
+    ul.appendChild(li);
+})
+
+
