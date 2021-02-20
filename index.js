2bbaf0c2b138d6e8f8a8b0629764f29b6885ee07
JDJ97 <jdj97@techie.com>
2021-02-20 15:30:36 -0500

    Added addToList Function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
+
+const addToList = () => {
+    let input = document.querySelector("#list-adder")
+    const ul = document.querySelector("ul")
+    inputText = input.value
+    li = document.createElement("li")
+    ul.appendChild(li)
+    li.textContent = inputText
+    
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
