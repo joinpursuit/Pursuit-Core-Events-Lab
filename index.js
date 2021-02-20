05b4b77a44f243b600ce0ab2d04144f4c0b9a772
Jeffrey <jeffreysebastian@pursuit.org>
2021-02-20 18:02:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+
+let input = document.querySelector("#toDoInput");
+let ul = document.querySelector("#newUl")
+const button = document.querySelector("#submitTodo");
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+    let li = document.createElement("li")
+    li.classList.add("todoList");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    const list = document.getElementsByClassName("todoList")
+})
+let newStyle = document.querySelector("#newUl")
+newStyle.addEventListener("click", (e)=>{
+    e.target.style.textDecoration = "line-through solid rgb(0,0, 0)"
+    
+
+
+})
+
