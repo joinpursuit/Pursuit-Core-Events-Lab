460427550a2f616a0189529c0064d7ded0876abe
Jonathan Bedon <bedonjonathan@gmail.com>
2021-02-20 18:01:02 -0500

    working on bonus

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-console.log("Code your solution!")
-let ul = document.getElementById("unordered-list")
-let form = document.querySelector("form")
+console.log("Code your solution!");
+let ul = document.getElementById("unordered-list");
+let form = document.querySelector("form");
 form.button.addEventListener("click", (event) => {
-    event.preventDefault();
-    let input = document.getElementById("text-input")
-    let input1 = input.value
-    let p = document.querySelector("p")
-    if(input1 === "") {
-        p.innerText = "Error. Todo cannot be empty"
-
+  event.preventDefault();
+  let input = document.getElementById("text-input");
+  let input1 = input.value;
+  let p = document.querySelector("p");
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  if (input1 === "") {
+    p.innerText = "Error. Todo cannot be empty";
+  } else {
+    btn.textContent = "Delete";
+    li.textContent = input1 + " ";
+    ul.appendChild(li);
+    li.appendChild(btn);
+    p.textContent = "";
+    form.reset();
+  }
+  ul.addEventListener("click", (event) => {
+      //debugger
+    if (event.target === li && event.target.style.textDecoration !== "line-through") {
+      event.target.style.textDecoration = "line-through";
+    }else{
+    event.target.style.textDecoration = "none";
     }
-    else {
-        let li = document.createElement("li")
-        let btn = document.createElement("button")
-        btn.textContent = "Delete"
-        li.textContent = input1
-        ul.appendChild(li)
-        li.appendChild(btn)
-        p.textContent = ""
-        form.reset();
-    }
-
-    ul.addEventListener("click", (event)  => {
-        event.target.style.textDecoration = "line-through"
-    })
-
-})
+  });
+  btn.addEventListener("click", () => {
+    li.parentNode.removeChild(li);
+  });
+});
