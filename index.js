59fceadcb19e820b50835e2c518dc921414c2edc
Durdona Djalilova <ddjalilova3@gmail.com>
2021-02-20 17:33:55 -0500

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Code your solution!")
+//  const addListItem = () => {
+//      const ul = document.querySelector("#list")
+//      let listItem = document.createElement("li")
+//      let input = document.querySelector("#input").value
+//      listItem.textContent = input
+//      ul.appendChild(listItem)
+//  }
+ 
+ 
+ document.addEventListener("DOMContentLoaded", () => {
+
+    //const button = document.querySelector("#add-to-do-list")
+    
+    const form = document.querySelector("form")
+    const p = document.querySelector("#error")
+    let input = document.querySelector("#input")
+    const ul = document.querySelector("#list")
+    let listItem
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+   
+        // const input = document.querySelector("#input").value
+        if(input.value === "") {
+            p.textContent = `Error. Todo cannot be empty`
+            // setTimeout(()=> {
+            //    }, 5000)
+        } else {
+            listItem = document.createElement("li")
+            listItem.textContent = input.value
+            ul.appendChild(listItem)
+            p.textContent = ""
+            input.value = ""
+            // listItem.textContent = input.value
+        }
+    })  
+    list.addEventListener("click", (e) => {
+        e.target.style.textDecoration = "line-through"
+        })
+
+})
