86ecd4c60b012dd6b9633f32b56cfc47bb5df92e
Kalilah Clarke <kalilah.clarke@gmail.com>
2021-02-20 17:27:23 -0500

    Working on Bonus

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const ul = document.querySelector("ul");
   const input = document.querySelector("input");
+  const p= document.querySelector("p")
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     const listItem = document.createElement("li");
-    listItem.textContent = input.value;
-    ul.appendChild(listItem);
+    if (input.value.length === 0){
+      p.textContent = `Error To Do Cannot Be Empty` 
+    }else{
+      listItem.textContent = input.value;
+      ul.appendChild(listItem);
+      p.textContent=""
+    }
+    input.value = ""
     listItem.addEventListener("click", (event) => {
-        listItem.style.textDecoration = "line-through"
+      listItem.style.textDecoration = "line-through"
     })
   });
 });
+
