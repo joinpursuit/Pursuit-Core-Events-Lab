1f4ddd799967858322653b4fc680a5cb9daf3777
Jordan Bobadilla <jordanboba08@gmail.com>
2021-02-21 17:42:56 -0500

    minor errors fixed

--- index.js
+++ index.js
@@ -16,34 +16,38 @@ document.addEventListener("DOMContentLoaded", (event) => {
     if (input.value === "") {
       p.textContent = "Error. Todo cannot be empty";
     } else {
+      p.textContent = ""
       const lines = input.value.split("\n");
       for (const line of lines) {
-        // create the li
-        const li = document.createElement("li");
-        li.textContent = line;
-        ul.appendChild(li);
-
-        // need to attach the button to each li as it is created
-        const button = document.createElement("button");
-        button.textContent = "Remove";
-        li.appendChild(button);
-
-        // EVENT LISTENERS
-        // click done to cross it out
-        li.addEventListener("click", () => {
-          if (li.style.textDecoration === "line-through") {
-            li.style.textDecoration = "none solid rgb(0, 0, 0)";
-            li.style.opacity = 1;
-          } else {
-            li.style.textDecoration = "line-through";
-            li.style.opacity = 0.3;
-          }
-        });
-
-        // click remove to delete
-        button.addEventListener("click", () => {
-          button.parentNode.remove();
-        });
+        if (line.length !== 0) {
+            
+            // create the li
+            const li = document.createElement("li");
+            li.textContent = line;
+            ul.appendChild(li);
+            
+            // need to attach the button to each li as it is created
+            const button = document.createElement("button");
+            button.textContent = "Remove";
+            li.appendChild(button);
+            
+            // EVENT LISTENERS
+            // click done to cross it out
+            li.addEventListener("click", () => {
+                if (li.style.textDecoration === "line-through") {
+                    li.style.textDecoration = "none solid rgb(0, 0, 0)";
+                    li.style.opacity = 1;
+                } else {
+                    li.style.textDecoration = "line-through";
+                    li.style.opacity = 0.3;
+                }
+            });
+            
+            // click remove to delete
+            button.addEventListener("click", () => {
+                button.parentNode.remove();
+            });
+        }
       }
     }
     // reset the input value to empty string
