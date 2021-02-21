113eacb53d80ec58cf97f7edc6df8ed769d85f78
Miguel Garcia <miguelgarcia@pursuit.org>
2021-02-21 17:33:44 -0500

    bonus except the last

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ form.button.addEventListener("click", (event) => {
   event.preventDefault();
   let input = document.getElementById("text-input");
   let input1 = input.value;
-  let li = document.createElement("li");
   let p = document.querySelector("p");
+  let li = document.createElement("li");
   let btn = document.createElement("button");
   if (input1 === "") {
     p.innerText = "Error. Todo cannot be empty";
@@ -18,21 +18,14 @@ form.button.addEventListener("click", (event) => {
     p.textContent = "";
     form.reset();
   }
-  ul.addEventListener("click", (event) => {
-      debugger
-    if (event.target.style.textDecoration = "none") {
-        debugger
-    event.target.style.textDecoration = "line-through";
-    }
-    else if (event.target.style.textDecoration = "line-through") {
-        debugger
-        event.target.style.textDecoration = "initial"
-    }
-  });
-  let li = document.querySelector
-  
-  
-    btn.addEventListener("click", () => {
+  btn.addEventListener("click", () => {
     li.parentNode.removeChild(li);
   });
+});
+ul.addEventListener("click", (event) => {
+  if (event.target.style.textDecoration !== "line-through") {
+    event.target.style.textDecoration = "line-through";
+  }else{
+  event.target.style.textDecoration = "none";
+  }
 });
