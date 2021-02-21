9dd638cf8097996714bc25c7b486477101dc3d10
poonam <poonam398@gmail.com>
2021-02-21 16:54:01 -0500

    events Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Code your solution!")
+
+
+document.addEventListener("DOMContentLoaded", function(){
+   const errors = document.querySelector('p')
+   const input = document.querySelector('input')
+   const unorderedList = document.querySelector("ul")
+
+   unorderedList.addEventListener("click", (event) =>{
+       // if(event.target.tagName === "LI")
+        event.target.style.textDecoration = "line-through"
+        
+    })
+        
+   document.querySelector('form').addEventListener("submit", (event)=>{
+            event.preventDefault()
+            if(!input.value){
+                let message = errors.textContent = "Error"
+                return message
+        }
+        errors.textContent = ""
+        
+        const listItems= document.createElement("li")
+        listItems.textContent = input.value
+        unorderedList.appendChild(listItems)
+
+    })  
+})
+        
+
+
