bafab33c246d596da15abb5ceef9b5fcbcce3918
Auredy Sanchez <auredysanchez@gmail.com>
2021-02-21 18:10:33 -0500

    bonus

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const p = document.querySelector("#error-msg");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = document.querySelector("#to-do-list");
-    if (input.value === "") {
+    const textarea = document.querySelector("#to-do-text");
+    // debugger;
+    if (textarea.value === "") {
       p.textContent = "Error. Todo cannot be empty";
     } else {
       p.textContent = "";
       let li = document.createElement("li");
-      li.textContent = input.value;
+      let button = document.createElement("button");
+      button.class = "delete-button";// mdn classlist and add
+      button.textContent = "Delete";
+      li.textContent = textarea.value;
       const ul = document.querySelector("ul");
       ul.appendChild(li);
+      li.appendChild(button);
+      button.addEventListener("click", (event) => {
+        //   debugger;
+        event.target.parentNode.remove();
+      });
     }
   });
   const ul = document.querySelector("ul");
   ul.addEventListener("click", (event) => {
-    // debugger;
     event.target.style.textDecoration = "line-through";
   });
 });
