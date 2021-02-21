86f8ca176a6499194bbb23c6986d7b009f9cf0c4
Teraesa  Metivier <teraesam@yahoo.com>
2021-02-21 14:35:34 -0500

    cleaned all code

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-//  const addListItem = () => {
-//      const ul = document.querySelector("#list")
-//      let listItem = document.createElement("li")
-//      let input = document.querySelector("#input").value
-//      listItem.textContent = input
-//      ul.appendChild(listItem)
-//  }
- 
- 
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const p = document.querySelector("#error");
+  let input = document.querySelector("#input");
+  const ul = document.querySelector("#list");
+  let listItem;
 
-    //const button = document.querySelector("#add-to-do-list")
-    
-    const form = document.querySelector("form")
-    const p = document.querySelector("#error")
-    let input = document.querySelector("#input")
-    const ul = document.querySelector("#list")
-    let listItem
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-   
-        // const input = document.querySelector("#input").value
-        if(input.value === "") {
-            p.textContent = `Error. Todo cannot be empty`
-            // setTimeout(()=> {
-            //    }, 5000)
-        } else {
-            listItem = document.createElement("li")
-            listItem.textContent = input.value
-            ul.appendChild(listItem)
-            p.textContent = ""
-            input.value = ""
-            // listItem.textContent = input.value
-        }
-    })  
-    list.addEventListener("click", (e) => {
-        e.target.style.textDecoration = "line-through"
-        })
-
-})
+    if (input.value === "") {
+      p.textContent = `Error. Todo cannot be empty`;
+    } else {
+      listItem = document.createElement("li");
+      listItem.textContent = input.value;
+      ul.appendChild(listItem);
+      p.textContent = "";
+      input.value = "";
+    }
+  });
+  list.addEventListener("click", (e) => {
+    e.target.style.textDecoration = "line-through";
+  });
+});
