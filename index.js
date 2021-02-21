ce7abd212b20eaef6409c8b001f5fde33d6e95b9
anjuhypp <anjuhypp@gmail.com>
2021-02-21 17:57:24 -0500

    complete 3 bonus exercises

--- index.js
+++ index.js
@@ -3,31 +3,44 @@ console.log("Code your solution!")
 document.addEventListener("DOMContentLoaded", () => {
     const ul = document.querySelector("ul");
     const form = document.querySelector("form");
+    
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-    const input = document.querySelector("input").value;
+    let textArea = document.querySelector("textarea");
     const p = document.querySelector("p");
-    if(input === "") {
+    if(textArea.value === "") {
         p.innerText = "Error: To-Do List cannot be empty";
     } else {
         let li = document.createElement("li");
-        li.textContent = input;
+        li.textContent = textArea.value;
         ul.appendChild(li);
         p.innerText = "";
+
+        let delButton = document.createElement("button");
+        delButton.innerText = "Delete";
+        li.append(delButton)
+
+        delButton.addEventListener("click", () => {
+            ul.removeChild(li)
+        })
+        
     }
+   
+    textArea.value = ""
+
     })
 
+    const removeStrikethrough = (event) => {
+        if(event.target.style.textDecorationLine === "line-through") {
+            event.target.style.textDecorationLine = "none"
+        } else {
+            event.target.style.textDecorationLine = "line-through"
+        }
+    }
+
     let list = document.querySelector("ul")
-    list.addEventListener("click", (event) => {
-        event.target.style.textDecorationLine = "line-through";
-    })
+    list.addEventListener("click", removeStrikethrough)
+
 })
 
 
-// let list = document.querySelector("li").textContent
-//     for(let el of list) {
-//         el.addEventListener("click", (event) => {
-//             event.target.style.textDecorationLine = "line-through";
-
- // input.document.querySelector("input").value = "";
- // input.reset()
