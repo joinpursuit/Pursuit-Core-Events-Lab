a66cae673bbbbae30aa08e87c012430d40b27438
Miguel Catala <miguelcatala27@yahoo.com>
2021-02-21 15:48:28 -0500

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+let ul = document.querySelector("#newUl")
+const button = document.querySelector("#submitTodo");
+let input = document.querySelector("#toDoInput");
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+    let li = document.createElement("li")
+    li.classList.add("todoList");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    const list = document.getElementsByClassName("todoList")
+})
+    let newStyle = document.querySelector("#newUl")
+newStyle.addEventListener("click", (e)=>{
+    e.target.style.textDecoration = "line-through solid rgb(0,0, 0)"
+
+
+
+})
