c77d8058659fd320a215d82e6bf63536b7a628c5
vrwatson <vanessawatson@pursuit.org>
2021-02-21 17:54:00 -0500

    finished bonus

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault(); //INVOKE () preventDefault()!!!
-    const input = document.querySelector("input");
-    if (input.value.length === 0) {
+    const input = document.querySelector("#to-Do-List");
+    let extendedInput = input.value.split("\n");
+    const ul = document.querySelector("ul");
+    // BONUS MULTIPLE LINE ENTRY IDEAS: the textarea tag creates a string as the value. Each entry has a space between the next. The logic @ the moment is to get the value of the input --> split it into an array --> and then either iterate through the array and into the "li" or push into a blank array and then interate that array into the "li".
+    if (extendedInput.length === 0) {
       const errorMessage = document.querySelector("p");
       errorMessage.textContent = "Error. Todo cannot be empty";
     } else {
-      const listItem = document.createElement("li");
-      const deleteButton = document.createElement("button");
-      listItem.textContent = input.value;
-      document.querySelector("ul").appendChild(listItem);
-      listItem.appendChild(deleteButton);
-      deleteButton.textContent = "Delete";
+      extendedInput.forEach((item) => {
+        const listItem = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        // console.log(item);
+        // debugger
+        listItem.textContent = item;
+        ul.appendChild(listItem);
+        listItem.appendChild(deleteButton);
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("del-button");
+        deleteButton.addEventListener("click", (event) => {
+          debugger;
+          // event.target.parentNode.removeChild(event.target);
+          event.path[1].parentNode.removeChild(event.path[1]);
+          // event.target.parentNode.removeChild(li);
+        });
+      })  
       
-      
-      deleteButton.addEventListener("click", (event) => {
-        event.parentNode.removeChild(event.parentNode.parentNode);
-          //   debugger;
-      });
 
+      
     }
     
     input.value = "";
@@ -36,8 +46,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const crossOut = document.querySelector("ul");
   crossOut.addEventListener("click", (event) => {
-    event.target.style.textDecoration = "line-through";
+    // event.target.style.textDecoration = "line-through";
+    if (event.target.style.textDecoration === "line-through") {
+      event.target.style.textDecoration = "none";
+    } else {
+      event.target.style.textDecoration = "line-through";
+    }
   });
+
+  
+
+
 });
 
-// e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+
