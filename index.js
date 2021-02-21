98954ee891600a18250cefa75a9e268caf46fe75
Jordan Bobadilla <jordanboba08@gmail.com>
2021-02-21 17:22:19 -0500

    Bonus Completed

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 console.log("Code your solution!");
 
 document.addEventListener("DOMContentLoaded", (event) => {
-	const input = document.querySelector("textarea");
-	const form = document.querySelector("form");
-	const ul = document.querySelector("ul");
-	const p = document.querySelector("p");
-	// const li = document.createElement("li");
-	// const button = document.createElement("button");
+  const input = document.querySelector("textarea");
+  const form = document.querySelector("form");
+  const ul = document.querySelector("ul");
+  const p = document.querySelector("p");
+  // const li = document.createElement("li");
+  // const button = document.createElement("button");
 
-	form.addEventListener("submit", (event) => {
-		//prevents form from submitting
-		event.preventDefault();
+  form.addEventListener("submit", (event) => {
+    //prevents form from submitting
+    event.preventDefault();
 
-		// check if empty string passed in
-		if (input.value === "") {
-			p.textContent = "Error. Todo cannot be empty";
-		} else {
-			// create the li
-			const li = document.createElement("li");
-			li.textContent = input.value;
-			ul.appendChild(li);
+    // check if empty string passed in
+    if (input.value === "") {
+      p.textContent = "Error. Todo cannot be empty";
+    } else {
+      const lines = input.value.split("\n");
+      for (const line of lines) {
+        // create the li
+        const li = document.createElement("li");
+        li.textContent = line;
+        ul.appendChild(li);
 
-			// need to attach the button to each li as it is created
-			const button = document.createElement("button");
-			button.textContent = "Remove";
-			li.appendChild(button);
-			
-			// EVENT LISTENERS
-			// click done to cross it out
-			li.addEventListener("click", () => {
-                if (li.style.textDecoration === "line-through") {
-                    li.style.textDecoration = "none solid rgb(0, 0, 0)"
-                    li.style.opacity = 1;
-                    
-                } else {
-                    li.style.textDecoration = "line-through";
-                    li.style.opacity = 0.3;
-                    
-                }
-			});
+        // need to attach the button to each li as it is created
+        const button = document.createElement("button");
+        button.textContent = "Remove";
+        li.appendChild(button);
 
-			// click remove to delete
-			button.addEventListener("click", () => {
-				button.parentNode.remove();
-			});
-		}
-		// reset the input value to empty string
-		input.value = "";
+        // EVENT LISTENERS
+        // click done to cross it out
+        li.addEventListener("click", () => {
+          if (li.style.textDecoration === "line-through") {
+            li.style.textDecoration = "none solid rgb(0, 0, 0)";
+            li.style.opacity = 1;
+          } else {
+            li.style.textDecoration = "line-through";
+            li.style.opacity = 0.3;
+          }
+        });
 
-		
-	});
+        // click remove to delete
+        button.addEventListener("click", () => {
+          button.parentNode.remove();
+        });
+      }
+    }
+    // reset the input value to empty string
+    input.value = "";
+  });
 });
 
 // ***TESTING
