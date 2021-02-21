fd236392cada5b732acfb6fe9bad83ec41bcd8b0
Kobe <tahiriadam@gmail.com>
2021-02-21 17:22:59 -0500

    completed final bonus question

--- index.js
+++ index.js
@@ -3,32 +3,34 @@ console.log("Code your solution!");
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const ul = document.querySelector("ul");
-  const input = document.querySelector("input");
-  const p= document.querySelector("p")
+  const input = document.querySelector("textarea");
+  const p = document.querySelector("p");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const listItem = document.createElement("li");
-    const button = document.createElement("button");
-    if (input.value.length === 0){
-      p.textContent = `Error: To-Do Submittion Cannot Be Empty` 
-    }else{
-      listItem.textContent = input.value;
-      ul.appendChild(listItem);
-      button.textContent = "Delete Item"
-      listItem.appendChild(button);
-      p.textContent= ""
+    if (input.value.length === 0) {
+      p.textContent = `Error: To-Do Submittion Cannot Be Empty`;
+    } else {
+      const lines = input.value.split("\n");
+      for (const line of lines) {
+        const listItem = document.createElement("li");
+        const button = document.createElement("button");
+        listItem.textContent = line;
+        ul.appendChild(listItem);
+        button.textContent = "Delete Item";
+        listItem.appendChild(button);
+        p.textContent = "";
+        listItem.addEventListener("click", (event) => {
+          if (listItem.style.textDecoration === "line-through") {
+            listItem.style.textDecoration = "none solid rgb(0,0,0)";
+          } else {
+            listItem.style.textDecoration = "line-through";
+          }
+        });
+        button.addEventListener("click", (e) => {
+          button.parentNode.remove(e);
+        });
+      }
     }
-    input.value = ""
-    listItem.addEventListener("click", (event) => {
-        if (listItem.style.textDecoration === "line-through") {
-            listItem.style.textDecoration = "none"
-        } else {
-            listItem.style.textDecoration = "line-through"
-        }
-    })
-    button.addEventListener("click", (e) => {
-        button.parentNode.remove(e)
-    })
+    input.value = "";
   });
 });
-
