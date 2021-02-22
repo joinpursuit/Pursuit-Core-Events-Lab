2d4020b455db580bb6cab584261f6d3217db7bec
Raymond Udeogu <rfizzle25@gmail.com>
2021-02-22 16:46:31 -0500

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Code your solution!")
+
+
+    document.addEventListener("DOMContentLoaded", () => {
+        let userList = document.querySelector("ul")
+        userList.addEventListener("submit", (event) => {
+            event.preventDefault()
+          const input = document.getElementById('user-todo').value
+          let newList = document.createElement('li')
+          newList.textContent = input
+            userList.appendChild(newList)
+        })
+      })
