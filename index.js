6a40b4b7a13e8b50e65c1918be8651d7599a0225
esayh <esay.hernandez@gmail.com>
2021-02-22 17:55:05 -0500

    Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Code your solution!")
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.querySelector('form')
+	form.addEventListener('submit', (event) => {
+		event.preventDefault()
+
+		let input = document.querySelector('input').value
+		document.querySelector('form').reset()
+
+		if (input.length === 0) {
+			let p = document.querySelector('p')
+			p.textContent = 'Error. Todo cannot be empty'
+		} else {
+			let li = document.createElement('li')
+			let button = document.createElement('button')
+			button.innerHTML = 'Delete'
+			let ul = document.querySelector('ul')
+			li.textContent = input
+			ul.appendChild(li)
+			li.appendChild(button)
+			let p = document.querySelector('p')
+			p.textContent = ''
+		}
+	})
+	let list = document.querySelector('ul')
+	list.addEventListener('click', (event) => {
+		event.target.style.textDecoration = 'line-through'
+	})
+})
