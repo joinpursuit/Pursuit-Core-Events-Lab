933982b33502da80a1bf130999e2871dcf6a72af
DamienYule <damienyule@gmail.com>
2021-02-23 15:09:05 -0500

    completed w/out bonus

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const p = document.querySelector('p')
     const form = document.querySelector('form')
-    const ul = document.querySelector("ul")
+    const ul = document.querySelector('ul')
+    
 
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener('submit', (event) => {
         event.preventDefault()
 
         let input = document.querySelector('#textInput').value
-        let newLItem = document.createElement("li")
-        newLItem.innerText = input
-        ul.appendChild(newLItem)
-        console.log(input)
+        if (input === "") {
+            p.innerHTML = `Please enter text`
+        } else {
+            p.innerHTML = ""
+            let newLItem = document.createElement('li')
+            newLItem.textContent = input
+            ul.appendChild(newLItem)
+        }
+        // console.log(ul)
 
     })
+    ul.addEventListener("click", (event) => {
+        if (event.target.style.textDecoration === "line-through") {
+            event.target.style.textDecoration = "none";
+        } else {
+            event.target.style.textDecoration = "line-through";
+        }
+    })
+
 
 })
