2a9af0d808704ef657e12fd4126e675847a84b60
Jeffrey <jeffreysebastian@pursuit.org>
2021-02-22 21:39:39 -0500

    Added CSS

--- index.js
+++ index.js
@@ -9,25 +9,31 @@ form.addEventListener("submit", (e) => {
     // debugger
     e.preventDefault();
     if (input.value === ""){
-        pTag.textContent = "String Error Cannot submit empty ToDo";
+        pTag.textContent = "Error! ToDo cannot be empty";
     }else{
         pTag.textContent =""
         let li = document.createElement("li");
-        // li.classList.add("todoList");
+        li.classList.add("todoList");
         li.textContent = input.value;
         ul.appendChild(li);
         let deleteButton = document.createElement("button");
         deleteButton.innerHTML = "Delete";
-        li.appendChild(deleteButton);
-        input.value =""
+        li.appendChild(deleteButton); //Implement a delete `button` next to each `li` that removes that `li` tag entirely.
+        input.value ="" //Have the input go back to empty after adding a new todo.
         deleteButton.addEventListener("click", (e)=>{
             deleteButton.parentNode.remove(e);
         })
+
         let newStyle = document.querySelector("#newUl");
         newStyle.addEventListener("click", (e) => {
-          e.target.style.textDecoration = "line-through solid rgb(0,0, 0)";
-          
-          
+            if(e.target.style.textDecoration === "none"){
+                e.target.style.textDecoration = "line-through solid rgb(0,0, 0)";
+                // debugger
+            } else {
+                e.target.style.textDecoration = "none";
+        //   debugger
+          //Clicking a todo that is crossed out (completed) uncrosses the todo. 
+            };
         });
     }
 });
