720b88c4ab936396da89160f450e1b3931c01291
cassidybeni <cassidybeni@pursuit.org>
2021-02-22 21:59:07 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let form = document.querySelector('form')
+    let ul = document.querySelector('#ul')
+    let p = document.querySelector('p')
+    
+    form.addEventListener('submit', event => { 
+        event.preventDefault()
+        
+        let input = document.querySelector('#input').value
+        
+        if(input === ""){
+            p.innerText = "Error. Todo cannot be empty"
+        } else {
+            let li = document.createElement('li')
+            li.innerText = input
+            ul.appendChild(li)
+        }
+    })
+    
+    ul.addEventListener('click', event => {
+        event.target.style.textDecoration = "line-through"
+    })
+
+})
