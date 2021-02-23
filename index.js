d533bf8fb79192690772692f81f207fc5bdd7442
Raymond Udeogu <rfizzle25@gmail.com>
2021-02-23 00:51:54 -0500

    almost done with lab

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-
+console.log('raymond')
     document.addEventListener("DOMContentLoaded", () => {
-        let userList = document.querySelector("ul")
-        userList.addEventListener("submit", (event) => {
+        let form = document.querySelector('form')
+        form.addEventListener("submit", (event) => {
             event.preventDefault()
           const input = document.getElementById('user-todo').value
+          console.log(input)
           let newList = document.createElement('li')
           newList.textContent = input
-            userList.appendChild(newList)
+            form.appendChild(newList)
+            //line 10 adds the text to the line
+            //next we want to make an if else statement testing if there is input
+                //if there is input the normal function is passed
+                //else an error message is placed in a p tag below form
         })
       })
