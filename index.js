521e46c8f51c731de8b04792fcea9165d4b9f114
DamienYule <damienyule@gmail.com>
2021-02-23 12:53:43 -0500

    Damien Yule

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const form = document.querySelector('form')
+    const ul = document.querySelector("ul")
+
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        let input = document.querySelector('#textInput').value
+        let newLItem = document.createElement("li")
+        newLItem.innerText = input
+        ul.appendChild(newLItem)
+        console.log(input)
+
+    })
+
+})
