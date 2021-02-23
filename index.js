8aa71cb51e22ec55165428b02f1e6278d5e3a16b
Durdona Djalilova <ddjalilova3@gmail.com>
2021-02-22 22:10:30 -0500

    finish bonus

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const p = document.querySelector("#error");
-  let input = document.querySelector("#input");
+  const input = document.querySelector("#input");
   const ul = document.querySelector("#list");
-  let listItem;
-
+  
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     if (input.value === "") {
       p.textContent = `Error. Todo cannot be empty`;
     } else {
-      listItem = document.createElement("li");
-      //listItem.textContent = input.value;
-      let newArray = input.value.split("\n");
-      newArray.forEach((el) => {
-        listItem = document.createElement("li");
-        listItem.textContent = el;
-        ul.appendChild(listItem);
-        let button = document.createElement("button");
-        button.innerHTML = "Delete";
-        ul.appendChild(button);
-        p.textContent = "";
-        input.value = "";
-      });
-    }
-  });
-
-  //ul.appendChild(listItem);
-  //       let button = document.createElement("button");
-  //       button.innerHTML = "Delete";
-  //       ul.appendChild(button);
-        button.addEventListener("click", (e) => {
-          e.preventDefault();
-          e.target.parentNode.removeChild(listItem);
-          e.target.parentNode.removeChild(button);
-        });
-        // p.textContent = "";
-        // input.value = "";
-    //   }
-    });
-  list.addEventListener("click", (e) => {
-    let liStyle = e.target;
-    if (liStyle.style.textDecoration === "line-through") {
-      liStyle.style.textDecoration = "none";
-    } else {
-      liStyle.style.textDecoration = "line-through";
+      let lines = input.value.split("\n");
+      for (let line of lines) {
+        if (line) {
+          let listItem = document.createElement("li");
+          listItem.textContent = line;
+          ul.appendChild(listItem);
+          let button = document.createElement("button");
+          button.innerHTML = "Delete";
+          listItem.appendChild(button);
+          button.addEventListener("click", (e) => {
+            button.parentNode.remove(e);
+          });
+          p.textContent = "";
+          listItem.addEventListener("click", (e) => {
+            let liStyle = e.target;
+            if (liStyle.style.textDecoration === "line-through") {
+              liStyle.style.textDecoration = "none";
+            } else {
+              liStyle.style.textDecoration = "line-through";
+            }
+          });
+          input.value = "";
+        }
+      }
     }
   });
 });
+
