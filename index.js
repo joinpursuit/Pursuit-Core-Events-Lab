8f211b03293f0bae61a6188f39d308e3b01495e1
Grace Bong <gracebong.park@gmail.com>
2021-02-23 17:16:07 -0500

    Completed Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector('form')
+    const list = document.querySelector('ul')
+    const error = document.querySelector('p')
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.querySelector('input').value
+        
+        if (input === "") {
+            error.textContent = 'Error. Todo cannot be empty'
+        } else {
+            createList()
+        }
+       
+        function createList () { 
+            let li = document.createElement('li')
+            li.textContent = input
+            list.appendChild(li)
+        }
+    })
+
+    list.addEventListener('click', event => {
+        event.target.style.textDecoration = 'line-through'
+    })
+})
