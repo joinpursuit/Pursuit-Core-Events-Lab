e596bb0b9b4fb67276e51a7ef3710d78a66e3820
DamienYule <damienyule@gmail.com>
2021-02-24 16:14:14 -0500

    Bonus too

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 //load dom 
 document.addEventListener('DOMContentLoaded', () => {
-
-//select tags from dom for manipulation     
+    
+    //select tags from dom for manipulation     
     const p = document.querySelector('p')
     const form = document.querySelector('form')
     const ul = document.querySelector('ul')
-    const li = document.querySelector('li')
+    const submitButton = document.querySelector('#btn')
     
-
-//create event listener on form variable
-    form.addEventListener('submit', (event) => {
+    //create event listener on form variable
+    submitButton.addEventListener('click', (event) => {
         event.preventDefault() //prevent reload
-    //get input value from text that is input by user
-        let input = document.querySelector('#textInput').value
-       //set conditions for no text and text in.  
+    //get input by user
+        let input = document.querySelector('textarea').value
+        inputPlus = input.split("\n")
+        console.log(input)
+        console.log(inputPlus[0])
+    //set conditions for text and no text. 
+     
         if (input === "") {
             p.innerHTML = `Please enter text`
         } else {
+            for(let i = 0 ; i <inputPlus.length ;i++){
             p.innerHTML = ""
             let newLItem = document.createElement('li') //CREATE new line item 
-            newLItem.textContent = input //set users input to text of that item 
+            newLItem.textContent = inputPlus[i] //set users input to text of that item 
             ul.appendChild(newLItem) // append our ul varialble 
+            
+            document.querySelector("form").reset('')//
+            //const li = document.querySelector('li')
+            const removeButton = document.createElement("button")
+            removeButton.textContent = `Remove`
+            newLItem.appendChild(removeButton)
+           
+            removeButton.addEventListener("click", (event) =>{
+                newLItem.remove(event)
+            })
         }
+        }
+        
         // console.log(ul)
-
+        
     })
-//create event listener on ul variable to cross out list item
+    //create event listener on ul variable to cross out list item
     ul.addEventListener("click", (event) => {
         if (event.target.style.textDecoration === "line-through") {
-            event.target.style.textDecoration = "none"
+            event.target.style.textDecoration = ""
+            event.target.style.backgroundColor = ""
         } else {
             event.target.style.textDecoration = "line-through"
-            event.target.style.backgroundColor = 'black'
+            event.target.style.backgroundColor = "blue"
         }
     })
-
+    
+    
+    const listItem = document.querySelector('li')
+    
+    //remove btn 
 
 })
