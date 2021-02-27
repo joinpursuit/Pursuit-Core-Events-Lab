ea6caef5b64f8324ba983fad70e767039b6a9b3e
Jonathan Bedon <bedonjonathan@gmail.com>
2021-02-26 19:47:15 -0500

    Complete

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let form = document.querySelector("form");
 form.button.addEventListener("click", (event) => {
   event.preventDefault();
   let inputAll = document.getElementById("text-input").value;
-  let input = inputAll //.split("\n")
-  console.log(input) 
+  let input = inputAll.split("\n")
+  for (const el of input) {
   let p = document.querySelector("p");
   let li = document.createElement("li");
   let btn = document.createElement("button");
-  if (input.length === 0) {
+  if (el.length === 0) {
     p.innerText = "Error. Todo cannot be empty";
   } else {
     btn.textContent = "Delete";
-    li.textContent = input + " ";
+    li.textContent = el + " ";
     ul.appendChild(li);
     li.appendChild(btn);
     p.textContent = "";
@@ -21,11 +21,15 @@ form.button.addEventListener("click", (event) => {
   btn.addEventListener("click", () => {
     li.parentNode.removeChild(li);
   });
+  ul.addEventListener("click", (event) => {
+    if (event.target.style.textDecoration !== "line-through") {
+      event.target.style.textDecoration = "line-through";
+    }else{
+    event.target.style.textDecoration = "none";
+    }
+  });
+}
+
 });
-ul.addEventListener("click", (event) => {
-  if (event.target.style.textDecoration !== "line-through") {
-    event.target.style.textDecoration = "line-through";
-  }else{
-  event.target.style.textDecoration = "none";
-  }
-});
+
+
