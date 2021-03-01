6d47661b03c7a615a1b16a1e5efbcfce7c41d96b
Jon Narine <jonnicwolf@gmail.com>
2021-03-01 14:19:03 -0500

    input = null | add defer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+
+const input = document.querySelector('#task-input')
+const taskList = document.querySelector('#task-list')
+const addNewTask = document.querySelector('#btn')
+console.log(input)
+
+addNewTask.addEventListener('click', (e) => {
+    e.preventDefault()
+    const newTask = document.createElement('li')
+    newTask.textContent = input.value
+    taskList.appendChild(newTask
+    if (!input.value) {
+        alert('Please enter a task before attempting to add to the list')
+    }
+    //console.log(e.childNode.target)
+})
+
+
+
