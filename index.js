ee4e69034f1497f548b42f36a79b21edfe866be1
Jon Narine <jonnicwolf@gmail.com>
2021-03-01 20:31:58 -0500

    attempts to change line-through

--- index.js
+++ index.js
@@ -2,24 +2,33 @@
 const input = document.querySelector('#task-input')
 const taskList = document.querySelector('#task-list')
 const addNewTask = document.querySelector('#btn')
-console.log(input)
+
+//const clickCount = 0
 
 addNewTask.addEventListener('click', (e) => {
     e.preventDefault()
+    clickCount++
     const newTask = document.createElement('li')
     newTask.textContent = input.value
     if (input.value==='') {
         alert('Please enter a task before attempting to add to the list')
-    } else {
+    } else {        
         taskList.appendChild(newTask)
-    }
-    //console.log(e.childNode.target)
+        input.value = ''
+    }    
 })
-
 
 taskList.addEventListener('click', (e) => {
-    e.target.style.textDecoration = 'line-through solid black'
+    const clickCount = 0
+    if (clickCount % 2 === 0) {
+        e.target.style.textDecoration = 'line-through solid black'        
+    } else if (clickCount % 2 !== 0) {
+        e.target.style.textDecoration = 'none'
+    }
 })
+
+//taskList.addEventListener('click', (e) => {
+//})
 
 
 
